c447f15959c5ab22c04883c646f75c72d2bd9bee
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-03-21 10:35:27 -0400

    Boolean for redux logger in prod

--- src/store/index.js
+++ src/store/index.js
@@ -18,13 +18,22 @@ const rootReducer = combineReducers({
   notification,
 });
 
-const logger = createLogger({
-  collapsed: true,
-  duration: true,
-});
+let middleware = [ReduxThunk];
+if (process.env.NODE_ENV !== 'production') {
+  const logger = createLogger({
+    collapsed: true,
+    duration: true,
+  });
+  middleware.push(logger)
+
+}
 
 // logger must be the last middleware in chain, otherwise
 // it will log thunk and promise, not actual actions
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(ReduxThunk, logger)); //eslint-disable-line
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(...middleware)
+);
 
 export default store;
