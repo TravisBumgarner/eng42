0e9e2cc2fedbc51973f38ecc758d5a48397db417
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-03-25 13:59:30 -0400

    Small change to homepage

--- src/views/Home/Home.styles.js
+++ src/views/Home/Home.styles.js
@@ -2,11 +2,17 @@ import styled from 'styled-components';
 
 import {
   DEFAULT_PAGE_WRAPPER,
+  media,
 } from "../../theme";
 
 const HomeWrapper = DEFAULT_PAGE_WRAPPER.extend`
   margin: 30vh auto 0;
   height: 70vh;
+  
+  ${media.TABLET`
+    margin-top: 15vh;
+    height: 85vh;
+  `};
 `;
 
 export {
