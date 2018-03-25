45bf5ea660612af84af2ac6365e23d1040c01341
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-03-25 12:33:49 -0400

    Added next/prev bg

--- src/components/Carousel/Carousel.styles.js
+++ src/components/Carousel/Carousel.styles.js
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
+import { transparentize } from 'polished';
 
 import FaAngleLeft from 'react-icons/lib/fa/angle-left';
 import FaAngleRight from 'react-icons/lib/fa/angle-right';
 
 import {
+  PRIMARY_COLOR,
   TERTIARY_COLOR,
+  SECONDARY_COLOR,
 } from '../../theme'
 
 const ICON_SIZE = 30;
@@ -22,24 +25,32 @@ const CarouselWrapper = styled.div`
 const PrevButton = styled(FaAngleLeft)`
   position: absolute;
   top:calc(50% - ${ICON_SIZE/2}px);
-  left: 0px;
+  left: 0;
   z-index: 999;
+  background-color: ${ transparentize(0.8, SECONDARY_COLOR) };
+  border-radius: 50%;
+  padding: 0 2px 2px 0;;
   
   &:hover{
     fill: ${TERTIARY_COLOR};
     cursor: pointer;
+    background-color: ${ transparentize(0.8, TERTIARY_COLOR) };
   }
 `;
 
 const NextButton = styled(FaAngleRight)`
   position: absolute;
   top:calc(50% - ${ICON_SIZE/2}px);
-  right: 0px;
+  right: 0;
   z-index: 999;
+  background-color: ${ transparentize(0.8, SECONDARY_COLOR) };
+  border-radius: 50%;
+  padding: 0 0 2px 2px;
   
   &:hover{
     fill: ${TERTIARY_COLOR};
     cursor: pointer;
+    background-color: ${ transparentize(0.8, TERTIARY_COLOR) };
   }
 `;
 
