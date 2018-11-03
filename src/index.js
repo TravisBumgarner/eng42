d9e9751cf38ab119f4451e35c2ef1f3e35044027
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-11-03 15:17:06 +0000

    (ui) Added sentry

--- src/index.js
+++ src/index.js
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import * as Sentry from '@sentry/browser';
 
 import App from './views/App/App';
 import store from './store';
 import { muiTheme } from "./theme";
 
-ReactDOM.render(
-  <Provider store={store}>
-      <BrowserRouter>
-          <MuiThemeProvider muiTheme={muiTheme}>
-            <App />
-          </MuiThemeProvider>
-      </BrowserRouter>
 
-  </Provider>,
+Sentry.init({
+ dsn: "https://96b0481bcf204abfaec06fb7ffc59ed8@sentry.io/1315175"
+});
+
+class SentryWrapper extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            error: null
+        }
+    }
+
+    componentDidCatch(error, errorInfo) {
+        this.setState({ error })
+        Sentry.withScope(scope => {
+            Object.keys(errorInfo).forEach(key => {
+                scope.setExtra(key, errorInfo[key])
+            })
+            Sentry.captureException(error)
+        })
+    }
+
+    render() {
+        if (this.state.error) {
+            return <Error500 />
+        } else {
+            return this.props.children
+        }
+    }
+}
+
+ReactDOM.render(
+  <SentryWrapper> 
+    <Provider store={store}>
+      <BrowserRouter>
+        <MuiThemeProvider muiTheme={muiTheme}>
+          <App />
+        </MuiThemeProvider>
+      </BrowserRouter>
+    </Provider>
+  </SentryWrapper>,
   document.getElementById('root'),
 );
