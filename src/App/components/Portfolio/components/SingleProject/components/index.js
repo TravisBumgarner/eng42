f0bc80c4e3f5a6270b58281c2604ba8a475e41f4
Travis Bumgarner <travis.bumgarner@gmail.com>
2019-03-27 23:26:06 -0400

    Dead code, functional code

--- src/App/components/Portfolio/components/SingleProject/components/index.js
+++
@@ -1,4 +0,0 @@
-import Carousel from './Carousel'
-import ProjectDetail from './ProjectDetail'
-
-export { Carousel, ProjectDetail }
--- src/App/components/Portfolio/index.js
+++ src/App/components/Portfolio/index.js
@@ -1,47 +1,19 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import projectActions from '../../../store/project/actions'
+import styled from 'styled-components'
+
+import { Card } from 'SharedComponents'
+import { DEFAULT_PAGE_WRAPPER } from 'Theme'
 
 import { ProjectsGrid } from './components'
 
-import { PortfolioWrapper, ProjectsCard } from './Portfolio.styles'
-
-export class Portfolio extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isProjectOpen: false
-        }
-    }
-
-    openProject = id => {
-        const { setSelectedProject } = this.props
-
-        setSelectedProject(id)
-        this.setState({ isProjectOpen: true })
-        window.scrollTo(0, 0)
-    }
-
-    closeProject = () => {
-        this.setState({ isProjectOpen: false })
-    }
-
-    render() {
-        return (
-            <PortfolioWrapper>
-                <ProjectsCard title="Portfolio">
-                    <ProjectsGrid openProject={this.openProject} />
-                </ProjectsCard>
-            </PortfolioWrapper>
-        )
-    }
+const Portfolio = () => {
+    return (
+        <Card>
+            <DEFAULT_PAGE_WRAPPER>
+                <ProjectsGrid />
+            </DEFAULT_PAGE_WRAPPER>
+        </Card>
+    )
 }
 
-export default connect(
-    state => ({
-        project: state.project.all[state.project.selected]
-    }),
-    {
-        setSelectedProject: projectActions.setSelectedProject
-    }
-)(Portfolio)
+export default Portfolio
