11328afaea169d910c9ca5feda5fc8d3d53592ce
Travis Bumgarner <travis.bumgarner@gmail.com>
2019-03-27 23:30:58 -0400

    bye dead code

--- src/sharedComponents/index.js
+++ src/sharedComponents/index.js
@@ -1,7 +1,6 @@
 import Card from './Card'
-import Thumbnail from './Thumbnail'
 import Paragraph from './Paragraph'
 import ExternalLink from './ExternalLink'
 import Error from './Error'
 
-export { ExternalLink, Paragraph, Thumbnail, Card, Error }
+export { ExternalLink, Paragraph, Card, Error }
