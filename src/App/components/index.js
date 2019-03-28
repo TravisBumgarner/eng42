d704d32284eadab9ddc9af2c75bc7d86fbc103fd
Travis Bumgarner <travis.bumgarner@gmail.com>
2019-03-27 23:21:27 -0400

    Removed Learn More

--- src/App/components/index.js
+++ src/App/components/index.js
@@ -1,8 +1,7 @@
 import Header from './Header'
 import Home from './Home'
-import LearnMore from './LearnMore'
 import NotFound from './NotFound'
 import Portfolio from './Portfolio'
 import SingleProject from './SingleProject'
 
-export { Header, Home, LearnMore, NotFound, Portfolio, SingleProject }
+export { Header, Home, NotFound, Portfolio, SingleProject }
