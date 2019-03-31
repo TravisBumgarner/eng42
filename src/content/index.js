fc54564360c8d7f4d7990c02fe3176097394f00e
Travis Bumgarner <travis.bumgarner@gmail.com>
2019-03-31 12:27:02 -0400

    Updated content

--- src/content/index.js
+++ src/content/index.js
@@ -33,5 +33,43 @@ import file_33 from './33.json'
 import file_34 from './34.json'
 import file_35 from './35.json'
 
+import skills from './skills.json'
+import categories from './categories.json'
 
- export default [file_1,file_2,file_3,file_4,file_5,file_6,file_7,file_8,file_9,file_10,file_11,file_12,file_13,file_14,file_15,file_16,file_17,file_18,file_19,file_20,file_21,file_22,file_23,file_24,file_25,file_26,file_27,file_29,file_30,file_31,file_32,file_33,file_34,file_35]
+export { skills, categories }
+export default [
+    file_1,
+    file_2,
+    file_3,
+    file_4,
+    file_5,
+    file_6,
+    file_7,
+    file_8,
+    file_9,
+    file_10,
+    file_11,
+    file_12,
+    file_13,
+    file_14,
+    file_15,
+    file_16,
+    file_17,
+    file_18,
+    file_19,
+    file_20,
+    file_21,
+    file_22,
+    file_23,
+    file_24,
+    file_25,
+    file_26,
+    file_27,
+    file_29,
+    file_30,
+    file_31,
+    file_32,
+    file_33,
+    file_34,
+    file_35
+]
