ba84532989b05e4db9c1d0b4ac7cc357d57a4ede
Travis Bumgarner <travis.bumgarner@gmail.com>
2019-03-31 23:19:08 -0400

    Bye bye dead code

--- webpack.js
+++ webpack.js
@@ -12,8 +12,7 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
-                    presets: ['@babel/preset-env', '@babel/preset-react'],
-                    plugins: [require('babel-plugin-transform-class-properties')]
+                    presets: ['@babel/preset-env', '@babel/preset-react']
                 }
             }
         ]
