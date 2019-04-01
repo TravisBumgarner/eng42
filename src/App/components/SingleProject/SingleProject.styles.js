e5407783a15868263a488022a85cc2283da72201
Travis Bumgarner <travis.bumgarner@gmail.com>
2019-03-31 22:46:34 -0400

    Dead Code

--- src/App/components/SingleProject/SingleProject.styles.js
+++ src/App/components/SingleProject/SingleProject.styles.js
@@ -1,7 +1,5 @@
 import styled from 'styled-components'
 
-import { Card } from 'SharedComponents'
-
 const SingleProjectWrapper = styled.div`
     justify-content: space-between;
     display: flex;
--- src/sharedComponents/index.js
+++ src/sharedComponents/index.js
@@ -1,7 +1,6 @@
-import Card from './Card'
 import Text from './Text'
 import Title from './Title'
 import ExternalLink from './ExternalLink'
 import Error from './Error'
 
-export { ExternalLink, Text, Card, Error, Title }
+export { ExternalLink, Text, Error, Title }
