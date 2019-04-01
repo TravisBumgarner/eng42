5cda716ed79b177cc875d8ead68f01529fb5b8fd
Travis Bumgarner <travis.bumgarner@gmail.com>
2019-03-31 21:48:55 -0400

    Fixed scrolling issue

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React, { Component } from 'react'
+import { Switch, Route, withRouter } from 'react-router-dom'
 
 import projects from 'Content'
 
@@ -8,28 +8,38 @@ import { GlobalStyle } from 'Theme'
 
 import { AppWrapper } from './App.styles'
 
-const App = () => {
-    const sortedProjects = projects.sort((a, b) => Date.parse(b.end_date) - Date.parse(a.end_date))
-    return (
-        <>
-            <GlobalStyle />
-            <AppWrapper>
-                <Header />
-                <Switch>
-                    <Route
-                        exact
-                        path="/"
-                        render={props => <Portfolio {...props} projects={sortedProjects} />}
-                    />
-                    <Route
-                        path="/project/:id"
-                        render={props => <SingleProject {...props} projects={sortedProjects} />}
-                    />
-                    <Route component={NotFound} />
-                </Switch>
-            </AppWrapper>
-        </>
-    )
+class App extends Component {
+    componentDidUpdate(prevProps) {
+        if (this.props.location.pathname !== prevProps.location.pathname) {
+            window.scrollTo(0, 0)
+        }
+    }
+
+    render() {
+        const sortedProjects = projects.sort(
+            (a, b) => Date.parse(b.end_date) - Date.parse(a.end_date)
+        )
+        return (
+            <>
+                <GlobalStyle />
+                <AppWrapper>
+                    <Header />
+                    <Switch>
+                        <Route
+                            exact
+                            path="/"
+                            render={props => <Portfolio {...props} projects={sortedProjects} />}
+                        />
+                        <Route
+                            path="/project/:id"
+                            render={props => <SingleProject {...props} projects={sortedProjects} />}
+                        />
+                        <Route component={NotFound} />
+                    </Switch>
+                </AppWrapper>
+            </>
+        )
+    }
 }
 
-export default App
+export default withRouter(App)
